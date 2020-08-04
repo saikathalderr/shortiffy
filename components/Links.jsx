a26436222e22944f9d1b126f6ef431b328dc8607
Saikat Halder <saikathalderr@gmail.com>
2020-08-04 19:29:42 +0530

    LInks UI FIxed

--- components/Links.jsx
+++ components/Links.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
-import { faCopy, faCalendarDay } from '@fortawesome/free-solid-svg-icons';
+import { faCopy, faCalendarDay, faTrashAlt, faClock } from '@fortawesome/free-solid-svg-icons';
 import { faCopy as faCopyOutline } from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Moment from 'react-moment';
 import { useRouter } from 'next/router';
+import { Button } from 'antd';
 
 function Links(props) {
   const router = useRouter();
@@ -23,32 +24,48 @@ function Links(props) {
       >
         <div className='w-full'>
           <span className='text-xs font-bold text-gray-400'>
-            <FontAwesomeIcon icon={faCalendarDay} className='mr-2' />
+            <FontAwesomeIcon icon={faCalendarDay} className='mr-1' />
             <Moment format='YYYY-MM-DD'>{props.linkData.createdAt}</Moment>
           </span>
+          <span className='ml-3 text-xs font-bold text-gray-400'>
+            <FontAwesomeIcon icon={faClock} className='mr-1' />
+            <Moment format='HH:MM A'>{props.linkData.createdAt}</Moment>
+          </span>
         </div>
-        <div>
+
+        <div className='flex'>
           <button>
             <FontAwesomeIcon
               icon={copy ? faCopy : faCopyOutline}
-              className='text-gray-500'
+              className='flex-1 text-gray-500'
               onClick={(e) => {
                 e.stopPropagation();
                 setcopy(!copy);
+                setTimeout(() => {
+                  setcopy(false);
+                }, 2000);
               }}
             />
           </button>
           <input
             readOnly={true}
             value={props.linkData.short_url}
-            className='font-bold text-xs text-black bg-transparent ml-2'
+            className='flex-1 font-bold text-xs text-black bg-transparent ml-2'
           />
+          {/* <Button
+            type='text'
+            danger
+            shape='circle'
+            icon={<FontAwesomeIcon icon={faTrashAlt} />}
+          /> */}
         </div>
+
         <input
           readOnly={true}
           value={props.linkData.long_url}
           className='w-full text-xs text-gray-400 bg-transparent'
         />
+
         {/* <div className='w-full'>
           <span className='text-xs text-gray-400'>
             <span className='font-bold'>By</span> Saikat Halder
