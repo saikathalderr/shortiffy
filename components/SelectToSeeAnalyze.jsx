97d8f65bea2a0f7d1b1f27036cf9bd4399a1f728
Saikat Halder <saikathalderr@gmail.com>
2020-08-08 19:13:04 +0530

    text currection

--- components/SelectToSeeAnalyze.jsx
+++ components/SelectToSeeAnalyze.jsx
@@ -11,7 +11,7 @@ function SelectToSeeAnalyze() {
           />
           <h1 className='theme-font text-black text-2xl'>
             Analyze your links <br /> Shorten, Share, Analyze your long link's.
-            <br /> <span className='text-orange-900'>Select any link to see stats.</span>
+            <br /> <span className='text-orange-900'>Select a link to see stats.</span>
           </h1>
         </div>
       </>
