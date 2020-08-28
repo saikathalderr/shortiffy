28459ddb3cde114ac6d9e1bb09ba1b5fb62b0c3f
Saikat Halder <saikathalderr@gmail.com>
2020-08-28 20:30:31 +0530

    loading UI Fix

--- components/Loading.jsx
+++ components/Loading.jsx
@@ -3,12 +3,12 @@ import { Spin } from 'antd';
 import { LoadingOutlined } from '@ant-design/icons';
 
 function Loading() {
-    const antIcon = <LoadingOutlined style={{ fontSize: 15 }} spin />;
+    const antIcon = <LoadingOutlined style={{ fontSize: 20 }} spin />;
 
     return (
       <>
         <div>
-          <div className='m-auto'>
+          <div className='text-center'>
             <h1 className='theme-font text-orange-900 text-6xl'>Shortiffy</h1>
             <h1 className='text-black font-black'>
               <Spin indicator={antIcon} className='mr-2 mb-2' />
